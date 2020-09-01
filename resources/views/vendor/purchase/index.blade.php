e617b581fe08d2c90440ccd610da42b43d71c536
Shujie <shujie001202@gmail.com>
2020-09-01 16:55:48 +0800

    modified

--- resources/views/vendor/purchase/index.blade.php
+++ resources/views/vendor/purchase/index.blade.php
@@ -11,9 +11,21 @@
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
-            
+            @foreach ($purchase as $purchase)
+            <tr>
+                <th>INV-{{$purchase ->invId}}</th>
+                <th><a href="/vendor/purchase/{{$purchase->id}}/edit" class="btn btn-default">Edit</a></th>
+                <th>
+                    {!! Form::open(["action" => ["VpurchaseInvoiceController@destroy", $purchase->id], "method" => "POST", "class" => "pull-right"]) !!}
+                        {{Form::hidden("_method", 'DELETE')}}
+                        {{Form::submit("Delete", ["class" => "btn btn-danger"])}}
+                    {!! Form::close() !!}
+                </th>
+            </tr>
+            @endforeach
         </table>
     @else
         <p>Your have no purchase invoice created</p>
     @endif
 @endsection
+
